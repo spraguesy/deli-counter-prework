2e4fa505f0cfa48b7d39ad92d4386fd5c9fcf7a8
Andrew Sprague <andrewsprague@outlook.com>
2018-08-13 13:35:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,12 @@ def take_a_number (line, name)
   line.push(name)
   puts "You are #{line.length} in line."
 end
+
+def now_serving (arr)
+  if arr.length == 0
+    puts "There is nobody waiting to be serverd!"
+  else
+    currently_serving = arr.shift()
+    puts currently_serving
+  end 
+end
