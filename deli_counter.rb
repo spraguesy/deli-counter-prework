7e341c32717d70f9c734da0e75358a0002a8cd20
Andrew Sprague <andrewsprague@outlook.com>
2018-08-13 13:33:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line (arr)
+  if arr.length == 0
+    puts "The line is currently empty."
+  else 
+    
+end
+
+def take_a_number (line, name)
+  line.push(name)
+  puts "You are #{line.length} in line."
+end
